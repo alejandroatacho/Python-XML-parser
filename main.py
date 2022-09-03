99a7db49084d3c3989c305392e37a8b91968c439
Hinamizawa <hinamizawa.github@gmail.com>
2022-09-02 22:20:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,62 @@
 import xml.etree.ElementTree as ET
+import os
 
-tree = ET.parse('main.xml')
+# Python variables for the XML file
+xml_file = "main.xml"
+xml_file_path = os.path.join(os.path.dirname(__file__), xml_file)
+tree = ET.parse(xml_file_path)
 root = tree.getroot()
 
+# Python variables to have fun with
+family = ''
+print(root.tag, root.attrib)
+
+# Python code space
+print("Python code space has initiated")
 for child in root:
-    print(child.tag, child.attrib)
-    for subchild in child:
-        print(subchild.tag, subchild.attrib)
-        for subsubchild in subchild:
-            print(subsubchild.tag, subsubchild.attrib)
-            if subsubchild in subchild == True:
-                print(subsubchild.text, subsubchild.tag,
-                      subsubchild.attrib, subsubchild.tail)
+    print(child.tag, child.attrib, child.text)
+    #print(child.attrib, child.text, child.tag)
+'''
+
+print(root.tag, root.attrib)
+
+
+def __init__():
+    for child in root:
+        print(child.tag, child.attrib)
+        for subchild in child:
+            print(subchild.tag, subchild.attrib)
+            for subsubchild in subchild:
+                print(subsubchild.tag, subsubchild.attrib)
+                if subsubchild in subchild == True:
+                    print(subsubchild.text, subsubchild.tag,
+                          subsubchild.attrib, subsubchild.tail)
+                else:
+                    print("It failed!!")
+
+
+__init__()
+'''
+
+# # XML python variables
+# tree = ET.parse('main.xml')
+# root = tree.getroot()
+# # view the XML file to see the structure
+# print(root.tag, root.attrib)
+
+
+# def __init__():
+#     for child in root:
+#         print(child.tag, child.attrib)
+#         for subchild in child:
+#             print(subchild.tag, subchild.attrib)
+#             for subsubchild in subchild:
+#                 print(subsubchild.tag, subsubchild.attrib)
+#                 if subsubchild in subchild == True:
+#                     print(subsubchild.text, subsubchild.tag,
+#                           subsubchild.attrib, subsubchild.tail)
+#                 else:
+#                     print("It failed!!")
+
+
+# __init__()
