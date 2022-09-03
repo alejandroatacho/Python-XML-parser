717e44b3c0bae08aec9860d816ee8f4e8e118e7f
Hinamizawa <hinamizawa.github@gmail.com>
2022-09-02 20:40:48 -0400

    update

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ root = tree.getroot()
 
 for child in root:
     print(child.tag, child.attrib)
+    for subchild in child:
+        print(subchild.tag, subchild.attrib)
+        for subsubchild in subchild:
+            print(subsubchild.tag, subsubchild.attrib)
+            if subsubchild in subchild == True:
+                print(subsubchild.text, subsubchild.tag,
+                      subsubchild.attrib, subsubchild.tail)
