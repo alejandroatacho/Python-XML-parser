99ef5adb4f5a6ebec8a9d56bc04c427d2682d6ee
Hinamizawa <hinamizawa.github@gmail.com>
2022-09-03 15:29:42 -0400

    Update holder.py

--- holder.py
+++ holder.py
@@ -17,24 +17,23 @@ class Holder:
         attribute()
 
 
-def apply():
-    # Set method to add attribute
-    root.set('launched', '932022')  # adds the attribute
+class run:
+    def apply():
+        # Set method to add attribute
+        root.set('launched', '932022')  # adds the attribute
 
-    tree.write(storage)  # writes the file
+        tree.write(storage)  # writes the file
 
     # apply()
-
-
-class run:
     def run_id():
         id = 1
         for investor in tree.findall('investor'):
+            # sets the id attribute in the investor tag as a string
             investor.set('id', str(id))
             id += 1
             print(id, investor.get('name'))
 
-            tree.write(storage)
+            tree.write(storage)  # save in the xml file
 
     def run():
         for child in root:
@@ -43,3 +42,31 @@ class run:
             for subchild in child:
                 print(subchild.tag, subchild.attrib, subchild.text)
                 print(len(subchild), f"subchild has been found\n")
+
+    def add_investor():
+        i = 0
+        for child in root:
+            # print("im working")
+            print(child.attrib, child.text, child.tag)
+
+            if 1 == 1:
+                print("start")
+                i = len(child) + 1
+                investor1 = ET.fromstring(
+                    '<investor> name="Hinamizawa"</investor> id="{val}" />' .format(val=i))  # creates the investor tag
+                root.append(investor1)
+                tree.write(storage)  # save in the xml file
+                print("Investor has been added")
+            else:
+                print("Investor has not been added")
+
+            for subchild in child:
+                print(subchild.attrib, subchild.text, subchild.tag)
+
+            else:
+                print("I broke there is no subchild")
+
+    if __name__ == '__main__':
+        # run_id()
+        # run()
+        add_investor()
