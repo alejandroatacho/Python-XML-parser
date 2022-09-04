65687b957dc5897c4c4d93ba7330c1f59385e59a
Hinamizawa <hinamizawa.github@gmail.com>
2022-09-03 20:10:11 -0400

    update

--- elementPy.py
+++ elementPy.py
@@ -6,12 +6,41 @@ storage = "views\sample.xml"
 
 print(root.tag, root.attrib)
 
-ET.dump(tree)
-
 
 def check():
     for elm in root.findall("."):
         print(elm.tag, elm.attrib)
 
 
-check()
+def country():
+    for elm in root.findall("country"):
+        print(elm.tag, elm.attrib)
+
+
+def specific_country():
+    for elm in root.findall("country[@name='Panama']"):
+       # print(elm.tag, elm.attrib)
+        print(elm.attrib)
+
+
+def rank():
+    for elm in root.findall("country/rank"):
+        print(elm.tag, elm.attrib)
+
+
+def direction():
+    for elm in root.findall("./country[@name='Panama']/neighbor[@direction='W']"):
+        print(elm.tag, elm.attrib)
+
+
+def value_change():
+    for elm in root.findall("./country[@name='Liechtenstein']"):
+        elm.set("name", "USA")
+        tree.write(storage)
+
+
+if __name__ == "__main__":
+    # check()
+    # country()
+    # direction()
+    value_change()
